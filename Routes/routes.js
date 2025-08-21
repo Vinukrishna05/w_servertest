9bffdd00a42f0d9e0807a2c9a49cd0f3f0804fd7
Vinukrishna05 <vinukrishna0508@gmail.com>
2025-08-21 15:03:01 +0530

    Added WhatsApp webhook in MVC structure 2

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,8 +4,8 @@ const testController=require('../Controllers/testController')
 
 const router=express.Router()
 
-router.get("/webhook", webhookController.verifyWebhook);
-router.post("/webhook", webhookController.receiveMessage);
+router.get("/webhook", testController.verifyWebhook);
+router.post("/webhook", testController.receiveMessage);
 
 router.post('/send-message',testController.startMessage)
 
